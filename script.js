7154a8f403f1d3b91601ca6a2cb1bef1004e3fbb
srthk31 <sarthak31122004m@gmail.com>
2026-07-04 18:14:01 +0530

    Feat:Complete counter with play, pause and reset functionalities

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // Timer / Counter
-let pauseBtn = document.createElement('button')
-pauseBtn.id = 'pause-btn'
-let pauseImg = document.createElement('img')
-pauseImg.src="res/pause.png"
-pauseBtn.appendChild(pauseImg)
-
 let resetBtn = document.createElement('button')
 resetBtn.id = 'reset-btn'
+resetBtn.addEventListener('click', resetCounter)
 let resetImg = document.createElement('img')
 resetImg.src="res/reset-icon.png"
+
 resetBtn.appendChild(resetImg)
 
 let counterSec = 0
 let counterMin = 0 
 let counterhr = 0
+let isRunning = false
 let interval;
+
+//Click handler
+function playpauseHandler(){
+    if(isRunning == false){
+        startCounter()
+    }else{
+        stopCounter()
+    }
+}
+
+// Start the timer
 function startCounter(){
+    if(isRunning === false){
         interval = setInterval(function(){
         counterSec++
         document.querySelector('#sec').innerHTML = `${counterSec}`
@@ -32,13 +41,28 @@ function startCounter(){
             }
         }
     },1000) 
-    document.querySelector('#play-btn').remove()
-    document.querySelector('.counter-options').appendChild(pauseBtn)
+
+    isRunning = true
+    document.querySelector('#play-pause-btn img').src = "res/pause.png"
     document.querySelector('.counter-options').appendChild(resetBtn)
+  }
 }
-document.querySelector('#play-btn').addEventListener('click',startCounter)
+
+document.querySelector('#play-pause-btn').addEventListener('click', playpauseHandler)
 
 // Pause and Reset Timer
 function stopCounter(){
     clearInterval(interval)
+    isRunning = false
+    document.querySelector('#play-pause-btn img').src = "res/play-icon.png"
+}
+function resetCounter(){
+    stopCounter()
+    isRunning = false
+    counterSec = 0
+    counterMin = 0 
+    counterhr = 0
+    document.querySelector('#sec').innerHTML = `${counterSec}`
+    document.querySelector('#min').innerHTML = `${counterMin}`
+    document.querySelector('#hr').innerHTML = `${counterhr}`
 }
